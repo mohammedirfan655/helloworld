f212935075611927ae211596e0367f718e3b8478
irfan-cloudnix <irfan@cloudnxi.com>
2017-02-25 13:10:32 +0530

    callback checks

--- src/android/com/irfan/plugins/HelloWorld.java
+++ src/android/com/irfan/plugins/HelloWorld.java
@@ -14,13 +14,26 @@ public class HelloWorld extends CordovaPlugin {
 
 	@Override
 	public boolean execute(String action, JSONArray args, final CallbackContext callbackContext) throws JSONException {
+			if ( action.equals(ACTION_SHOW_EVENT) ) {
+				show(args.toString(),callbackContext);
+				return true;
+			}
+
+/*
 		CharSequence text = "This is my first cordova plugin";
 		int duration = Toast.LENGTH_SHORT;
 
 		Toast toast = Toast.makeText(cordova.getActivity().getApplicationContext(), args.toString(), duration);
 		toast.show();
-
-		return true;
+*/
+		return false;
 	}
 
+	private void show(String message, CallbackContext callbackContext) {
+		if (message != null && message.length() > 0) {
+		    callbackContext.success(message);
+		} else {
+		    callbackContext.error("Expected one non-empty string argument.");
+		}		
+	}
 }
